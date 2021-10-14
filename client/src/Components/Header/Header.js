471ebf68e2996f92ce40a93f27fa6c172af80231
heyaayush.py@gmail.com <heyaayush.py@gmail.com>
2021-10-14 20:20:59 +0530

    Nav Bar Added

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,24 +1,42 @@
 import React from "react"
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import './Header.css'
 
 const Header = () => {
     return (
-        <div className='nav-bar'>
-            <h3> Baniya-Trade </h3>
-            <ul className='nav'>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/signup'>Sign Up</Link>
-                </li>
-                <li>
-                    <Link to='/login'>Login</Link>
-                </li>
-            </ul>
-        </div>
+        <nav className='navbar navbar-expand-lg navbar-mainbg'>
+            <NavLink className="navbar-brand" to='/'> 
+                Baniya-Trade 
+            </NavLink>
+            <button 
+                className="navbar-toggler"
+                type="button" data-toggle="collapse" 
+                data-target="#navbarSupportedContent" 
+                aria-controls="navbarSupportedContent" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation">
+                <i class="fas fa-align-justify"></i>
+            </button>
+            <div className="collapse navbar-collapse" 
+                id="navbarSupportedContent">
+                <ul className='navbar-nav ml-auto'>
+                    <div className="hori-selector">
+                        <div className="left"></div>
+                        <div className="right"></div>
+                    </div>
+                    <li className="nav-item active">
+                        <NavLink to='/'>Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/signup'>Sign Up</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/login'>Login</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
-export default Header;
+export default Header; 
