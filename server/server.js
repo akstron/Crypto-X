ee40583e4e8842d91e7e7a3d2c5e41fa2203f4e9
akstron <62210712+akstron@users.noreply.github.com>
2021-10-14 13:53:56 +0530

    login completed

--- server/server.js
+++ server/server.js
@@ -1,10 +1,35 @@
 require('dotenv').config();
 const express = require('express');
-require('./config/connection');
+const passport = require('passport');
+const session = require('express-session');
 const app = express();
 
+require('./config/passport');
+require('./config/connection');
 const userAuthRouter = require('./routers/userAuthRouter');
 
+const sessionStore = require('connect-mongo').create({
+    mongoUrl: process.env.MONGO_DB_URI
+});
+
+app.use(
+    session({
+      secret: process.env.SECRET,
+      resave: true,
+      saveUninitialized: true, 
+      store: sessionStore,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+      }
+    })
+);
+
+/**
+ * TODO: Setting up cors
+ */
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 
 app.use(userAuthRouter);
