d14f0360a88838fb838a41ac9e8ae4a8cf9c8d2a
akstron <62210712+akstron@users.noreply.github.com>
2021-10-14 19:33:04 +0530

    Header updated

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,23 +1,24 @@
-import React from "react"
-import {Link} from 'react-router-dom'
-import './Header.css'
-
-const Header = () => {
-    return (
-        <div className='nav-bar'>
-            <h3> Baniya-Trade </h3>
-            <ul className='nav'>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/signup'>Sign Up</Link>
-                </li>
-                <li>
-                    <Link to='/login'>Login</Link>
-                </li>
-            </ul>
-        </div>
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+import {ReactComponent as Logo} from '../../assets/instagram.svg';
+import {ReactComponent as Home} from '../../assets/home.svg';
+import {ReactComponent as Explore} from '../../assets/explore.svg';
+import './header.css';
+const Header = () =>{
+    return(
+        <nav>
+            <div className='div-header'>
+                <div className='div-svg'>
+                    <Logo/>
+                </div>
+                <h2>Baniya Trade</h2>
+                <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
+                    <NavLink exact to='/' activeClassName='active'><Home className='div-svg'/></NavLink>
+                    <NavLink exact to='/explore' activeClassName='active'><Explore className='div-svg'/></NavLink>
+                    <button className='button-header'>Log out</button>
+                </div>
+            </div>
+        </nav>
     )
 }
 
