46e4ed2e74e3988d62eb38b7be6545649da70b51
heyaayush.py@gmail.com <heyaayush.py@gmail.com>
2021-10-16 20:31:54 +0530

    Profile Page Modified

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -13,6 +13,7 @@ import ValindationPage from "../Pages/ValindationPage";
 
 function App() {
   //Set/Fetch User Here ->
+  //Better to store in local Storage
   const [User,setUser]=useState({uId:21505341,uName:'Aayush',emailId:'aayushshandilya80@gmail.com'});
   console.log(User)
 
@@ -23,7 +24,7 @@ function App() {
         <Route path="/" component={HomePage} exact/>
         <Route path="/Market" component={Market}/>
         {/* Protected Route */}
-        <Route path="/Profile">{(User!==undefined)?(<ProfilePage User={User}/>):(<Redirect to='/login'/>)}</Route>
+        <Route path="/Profile">{(User!==undefined)?(<ProfilePage {...User}/>):(<Redirect to='/login'/>)}</Route>
         <Route path="/login" component={ LoginPage} setUser={setUser}/>
         <Route path="/signup" component={SignUpPage} setUser={setUser}/>
         <Route path="/validation/:emailId" component={ValindationPage}/>
--- client/src/Components/Pages/ProfilePage.js
+++ client/src/Components/Pages/ProfilePage.js
@@ -1,8 +1,38 @@
+import { FloatingLabel, Container,Row,Col,Form ,Button} from 'react-bootstrap';
+import './ProfilePage.css'
 //TODO:
-const ProfilePage = () => {
+const profilePic='https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg';
+const Message="Profile Page";
+
+const ProfilePage = ({uName,emailId}) => {
+    const changeProfileImage=()=>{}
+    const UpdateProfileHandler=()=>{}
     return (
-        <div>
-            <h3>Profile Page</h3>
+        <div className='profile-container'>
+            <Form className="form">  
+                <h2>User Profile</h2>
+                {/* <p>{Message}</p> */}
+                <img src={profilePic} alt="profils pic" height='215px'/>
+                <Form.Group controlId="formCategory4">
+                    <FloatingLabel>Profile Image</FloatingLabel>
+                    <Form.Control type="file" name="profileImage" onChange={changeProfileImage}/>
+                </Form.Group>
+                {/* <p>{Message}</p> */}
+                <Form.Group controlId="formCategory1">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" defaultValue={uName} readOnly/>
+                </Form.Group>
+                <Form.Group controlId="formCategory2">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" defaultValue={emailId} readOnly/>
+                </Form.Group>
+                <Form.Group controlId="formCategory3">
+                    <Form.Label>Phone : </Form.Label>
+                    <Form.Control type="phone" defaultValue={"+91-9424161813"}/>
+                </Form.Group>
+
+                <Button variant="primary" onClick={UpdateProfileHandler}>Update Profile</Button>
+            </Form>
         </div>
     )
 }
