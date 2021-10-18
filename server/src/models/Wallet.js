8a3112710844eed2ae2d5f353fcafa2276a4a069
akstron <62210712+akstron@users.noreply.github.com>
2021-10-18 11:41:08 +0530

    server side money transfer added

--- server/src/models/Wallet.js
+++ server/src/models/Wallet.js
@@ -3,6 +3,22 @@ const mongoose = require('mongoose');
 const walletSchema = mongoose.Schema({
     coins: {
         type: Object
+    }, 
+
+    transaction: {
+        type: Object
+    },
+
+    costPrice: {
+        type: Number,
+        require: true,
+        default: 0
+    }, 
+
+    balance: {
+        type: Number, 
+        require: true,
+        default: 0
     }
 }, 
 {
