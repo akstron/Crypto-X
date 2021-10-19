d07ac70bcaeb7c6eeb0872b63bad09694aaacb74
akstron <62210712+akstron@users.noreply.github.com>
2021-10-19 11:47:42 +0530

    adding testing route

--- server/src/routers/userUtilityRouter.js
+++ server/src/routers/userUtilityRouter.js
@@ -20,5 +20,39 @@ EditUser);
 
 router.put('/transaction', IsAuthenticated, IsVerified, Transaction);
 
+router.get('/check', (req, res) => {
+    res.json({
+        data: [
+            {
+                Id:1,
+                ImgURL :"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/BTC_Logo.svg/1200px-BTC_Logo.svg.png",
+                CoinTitle:'bitcoin',
+                CoinName:'Bitcoin',
+                CoinDetails:'A cryptocurrency, crypto-currency, or crypto is a collection of binary data which is designed to work as a medium of exchange wherein individual coin ownership records are stored in a ledger which is a computerized database using strong cryptography to secure transaction records.',
+                CoinPrice:25701.50,
+                CoinGrowth_24:0.51,
+            },
+            {
+                Id:2,
+                ImgURL :"https://assets.gadgets360cdn.com/img/crypto/ethereum-og-logo.png",
+                CoinTitle:'Ethereum',
+                CoinName:'ethereum',
+                coinDetails:'Ethereum is a decentralized, open-source blockchain with smart contract functionality. Ether is the native cryptocurrency of the platform. Amongst cryptocurrencies, Ether is second only to Bitcoin in market capitalization. Ethereum was conceived in 2013 by programmer Vitalik Buterin.',
+                CoinPrice:25701.50,
+                CoinGrowth_24:-0.21,
+            },
+            {
+                Id:3,
+                ImgURL :"https://assets.gadgets360cdn.com/img/crypto/dogecoin-og-logo.png",
+                CoinTitle:'Dogecoin',
+                CoinName:'digecoin',
+                CoinDetails:'Dogecoin is a cryptocurrency created by software engineers Billy Markus and Jackson Palmer, who decided to create a payment system as a "joke", making fun of the wild speculation in cryptocurrencies at the time. Despite its satirical nature, some consider it a legitimate investment prospect.',
+                CoinPrice:25701.50,
+                CoinGrowth_24:0.1,
+            },
+        ]
+    });
+})
+
 
 module.exports = router;
