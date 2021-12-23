0d0bc8f72301ab3a7b952e7e1d7d0fcd475710f4
heyaayush.py@gmail.com <heyaayush.py@gmail.com>
2021-12-23 11:09:23 +0530

    Portfolio Donut ZChart Added

--- client/src/Components/Portfolio/PortfolioPage.js
+++ client/src/Components/Portfolio/PortfolioPage.js
@@ -1,6 +1,7 @@
-import React,{useState,useEffect} from 'react'
-import { Typography,Divider,Row,Col,Statistic } from 'antd';
-import { ArrowUpOutlined,ArrowDownOutlined,LoadingOutlined,ExclamationCircleOutlined} from '@ant-design/icons';
+import React, { useState, useEffect } from 'react'
+import { Typography, Divider, Row, Col, Statistic } from 'antd';
+import { ArrowUpOutlined, ArrowDownOutlined, LoadingOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import PieDonut from '../Utils/PieDonut'
 
 import portfolioIcon from '../../Images/portfolioIcon.png';
 import coinIcon from '../../Images/money-bag.png';
@@ -11,114 +12,91 @@ import axios from 'axios';
 
 const { Title } = Typography;
 
-const coinGrowth=[
-        {
-            id:1,
-            name:'Bitcoin',
-            costPrice:245,
-            iconUrl:'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-            netWorth:1254,
-            totalGain:125,
-            growth:.25,
-        },
-        {
-            id:2,
-            name:'Bitcoin',
-            costPrice:245,
-            iconUrl:'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-            netWorth:1254,
-            totalGain:125,
-            growth:.25,
-        },
-        {
-            id:3,
-            name:'Bitcoin',
-            costPrice:245,
-            iconUrl:'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-            netWorth:1254,
-            totalGain:125,
-            growth:.25,
-        },
-    ]
+const getCoinsName=(portfolio)=>(portfolio.data.coins.map((coin,id)=>(coin.coinType)))
+const getInvestment=(portfolio)=>(portfolio.data.coins.map((coin,id)=>(coin.costPrice)))
+const getCoinsSale=(portfolio)=>(portfolio.data.coins.map((coin,id)=>(coin.sellPrice)))
+
 // 3 Case : not verified,no balance,no coins
 const Test = () => {
 
-    const [portfolio,setPortfolio]=useState({data:undefined,isFetching:true});
+    const [portfolio, setPortfolio] = useState({ data: undefined, isFetching: true });
 
-    const getPortfolio=()=>{
+    const getPortfolio = () => {
         const portfolioRoute = process.env.REACT_APP_BACKEND + '/getPortfolio';
-        
-        axios.get(portfolioRoute, {withCredentials: true}).then(res => {
+
+        axios.get(portfolioRoute, { withCredentials: true }).then(res => {
             console.log(res['data']);
-            
-            if(res['data']['portfolio']){
+
+            if (res['data']['portfolio']) {
                 setPortfolio({
-                    data:res.data.portfolio,
-                    isFetching:false
+                    data: res.data.portfolio,
+                    isFetching: false
                 });
             }
         }).catch(error => {
             console.log(error);
             setPortfolio({
-                data:undefined,
-                isFetching:false
+                data: undefined,
+                isFetching: false
             });
         })
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getPortfolio();
-    },[])
+    }, [])
 
     return (
         <div>
             <div className="portfolio-div">
                 <div className="portfolio-heading-div">
                     <Title level={2}>
-                        <img src={portfolioIcon} alt='ImgIcon' height={'50px'} style={{margin:'0rem .8rem '}}/>
+                        <img src={portfolioIcon} alt='ImgIcon' height={'50px'} style={{ margin: '0rem .8rem ' }} />
                         Porfolio
                     </Title>
                 </div>
-                <div className="portfolio-stats" style={{textAlign:"center"}}>
+                <div className="portfolio-stats" style={{ textAlign: "center" }}>
                     <Divider orientation="right">
                         <Title level={4}>
-                            <img src={WalletIcon} alt='ImgIcon' height={'40px'} style={{margin:'0rem .8rem '}}/>
+                            <img src={WalletIcon} alt='ImgIcon' height={'40px'} style={{ margin: '0rem .8rem ' }} />
                             Wallet
                         </Title>
                     </Divider>
 
-                    {(portfolio.isFetching)?(
+                    {(portfolio.isFetching) ? (
                         <>
-                            <LoadingOutlined style={{margin:"2rem auto",fontSize:"xxx-large"}}/>
+                            <LoadingOutlined style={{ margin: "2rem auto", fontSize: "xxx-large" }} />
                         </>
-                    ):(
+                    ) : (
                         <>
                             <Row>
-                                <Col xs={{span:24}} md={{span:12}} xl={{span:6}}><Statistic title="Total Investment ($)" value={millify(portfolio.data.totalCostPrice)}/></Col>
-                                {/* <Col xs={{span:24}} md={{span:12}} xl={{span:6}}><Statistic title="Net Worth ($)" value={millify(portfolio.data.1256)}/></Col> */}
-                                <Col xs={{span:24}} md={{span:12}} xl={{span:6}}><Statistic title="Total Sale ($)" value={millify(portfolio.data.totalSellPrice)}/></Col>
-                                <Col xs={{span:24}} md={{span:12}} xl={{span:6}}><Statistic title="Growth" value={portfolio.data.totalPercentGrowth} precision={2} valueStyle={(portfolio.data.totalPercentGrowth>0)?({ color: '#3f8600' }):({color: 'red'})} prefix={(portfolio.data.totalPercentGrowth>0)?(<ArrowUpOutlined/>):(<ArrowDownOutlined/>)} suffix="%"/></Col>
-                            </Row>    
+                                <Col xs={{ span: 24 }} md={{ span: 12 }} xl={{ span: 6 }}><Statistic title="Total Investment ($)" value={millify(portfolio.data.totalCostPrice)} /></Col>
+                                <Col xs={{span:24}} md={{span:12}} xl={{span:6}}><Statistic title="Net Worth ($)" value={millify(1256)}/></Col>
+                                <Col xs={{ span: 24 }} md={{ span: 12 }} xl={{ span: 6 }}><Statistic title="Total Sale ($)" value={millify(portfolio.data.totalSellPrice)} /></Col>
+                                <Col xs={{ span: 24 }} md={{ span: 12 }} xl={{ span: 6 }}><Statistic title="Growth" value={portfolio.data.totalPercentGrowth} precision={2} valueStyle={(portfolio.data.totalPercentGrowth > 0) ? ({ color: '#3f8600' }) : ({ color: 'red' })} prefix={(portfolio.data.totalPercentGrowth > 0) ? (<ArrowUpOutlined />) : (<ArrowDownOutlined />)} suffix="%" /></Col>
+                                <Col xs={{ span: 24 }} md={{ span: 12 }}><PieDonut Heading="Investment" CoinsName={getCoinsName(portfolio)} CoinsData={getInvestment(portfolio)} /></Col>
+                                <Col xs={{ span: 24 }} md={{ span: 12 }}><PieDonut Heading="Sales" CoinsName={getCoinsName(portfolio)} CoinsData={getCoinsSale(portfolio)} /></Col>
+                            </Row>
                         </>
                     )}
-                    
+
                 </div>
-                <div className="portfolio-coins" style={{textAlign:"center"}}>
-                    
+                <div className="portfolio-coins" style={{ textAlign: "center" }}>
+
                     <Divider orientation="left">
                         <Title level={4}>
-                            <img src={coinIcon} alt='ImgIcon' height={'40px'} style={{margin:'0rem .8rem '}}/>
+                            <img src={coinIcon} alt='ImgIcon' height={'40px'} style={{ margin: '0rem .8rem ' }} />
                             Coins Performance
                         </Title>
                     </Divider>
-                    {(portfolio.isFetching)?(
+                    {(portfolio.isFetching) ? (
                         <>
-                            <LoadingOutlined style={{margin:"2rem auto",fontSize:"xxx-large"}}/>
+                            <LoadingOutlined style={{ margin: "2rem auto", fontSize: "xxx-large" }} />
                         </>
-                    ):(
-                        <PortfolioCard coinGrowth={portfolio.data.coins}/>
+                    ) : (
+                        <PortfolioCard coinGrowth={portfolio.data.coins} />
                     )}
-                    <Divider/>
+                    <Divider />
                 </div>
             </div>
         </div>
