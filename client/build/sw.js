1861a52d605304fab3887ce8d419952b090e75aa
heyaayush.py@gmail.com <heyaayush.py@gmail.com>
2021-12-24 21:17:07 +0530

    Build Updated

---
+++ client/build/sw.js
@@ -0,0 +1,48 @@
+let cacheData="appV1";
+
+this.addEventListener("install",(event)=>{
+    event.waitUntil(
+        caches.open(cacheData).then((cache)=>{
+            cache.addAll([
+                '/static/js/main.chunk.js',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js',
+                '/index.html',
+                '/favicon.ico',
+                '/manifest.json',
+                '/static/js/vendors~main.chunk.js',
+                '/logo192.png ',
+                '/'
+            ])
+        })
+    )
+})
+
+this.addEventListener("fetch",(event)=>{
+
+    if(!navigator.onLine){
+        if(event.request.url === "http://localhost:3000/static/js/main.chunk.js"){
+            event.waitUntil(
+                this.registration.showNotification("Baniya-Trade",{
+                    body:"Looks like we just lost you. Please check your connection.",
+                })
+            )
+        }
+        event.respondWith(
+            caches.match(event.request).then((resp)=>{
+                if(resp){
+                    return resp;
+                }
+            })
+        )
+    }
+})
+
+this.addEventListener("push", e => {
+  const data = e.data.json();
+  console.log("Push Recieved...");
+  this.registration.showNotification(data.title, {
+    body: "Notified by Traversy Media!",
+    icon: "http://image.ibb.co/frYOFd/tmlogo.png"
+  });
+});
