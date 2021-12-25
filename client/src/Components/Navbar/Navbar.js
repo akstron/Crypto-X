e78f999d66ac4f69ed876b8c0358be9a24a5ded9
heyaayush.py@gmail.com <heyaayush.py@gmail.com>
2021-12-25 13:16:09 +0530

    Order Card Crash issue Fixed

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -88,7 +88,7 @@ const Navbar = () => {
                                 <Menu.Item key="6" icon={<BankOutlined />}>
                                     <Link to='/BankOptions'>Payment Options</Link>
                                 </Menu.Item>
-                                <Menu.Item key="7" icon={<ShoppingOutlined />} onClick={refreshPage}>
+                                <Menu.Item key="7" icon={<ShoppingOutlined />}>
                                     <Link to='/BuySell'>BuySell</Link>
                                 </Menu.Item>
                             </SubMenu>
