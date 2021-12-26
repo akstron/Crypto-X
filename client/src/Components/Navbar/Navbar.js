33e50f50341ae6da42c075f3c2fcf1376b34c3df
heyaayush.py@gmail.com <heyaayush.py@gmail.com>
2021-12-26 11:41:20 +0530

    NavBar Fixe

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,35 +1,18 @@
 import React,{ useContext,useState,useEffect } from 'react'
-import axios from 'axios';
-import { Button,Menu,Typography,Avatar } from 'antd';
+import { Button,Typography,Avatar } from 'antd';
 import { Link } from "react-router-dom";
-import { HomeOutlined, ProfileOutlined,DollarOutlined,UserAddOutlined, 
-        LoginOutlined,BulbOutlined,ShoppingOutlined,LogoutOutlined,
-        UserOutlined,MenuOutlined,BankOutlined,
-        TeamOutlined} from '@ant-design/icons';
-import icon from '../../Images/main-logo.png'
+import { MenuOutlined } from '@ant-design/icons';
+import MenuPanel from "./MenuPanel";
+import icon from '../../Images/main-logo.png';
 
-// !important Change Back to App
 import { UserContext } from '../../App';
 
-const { SubMenu } = Menu;
 
 const Navbar = () => {
     const [activeMenu, setActiveMenu] = useState(true);
     const [screenSize, setScreenSize] = useState(undefined);
     
     const User = useContext(UserContext);
- 
-    const logOut=async ()=>{
-        console.log("Loging Out")
-        const userRoute = process.env.REACT_APP_BACKEND + '/logout';
-        await axios.post(userRoute, {},{withCredentials: true}).then(res => {
-            console.log(res);
-            console.log("Log Out Clicked !");
-            window.location.reload(false);
-        }).catch(error => {
-            console.log(error);
-        })
-    }
 
     useEffect(() => {
         const handleResize = () => setScreenSize(window.innerWidth);
@@ -43,74 +26,29 @@ const Navbar = () => {
 
     useEffect(() => {
         if (screenSize <= 800) {
-        setActiveMenu(false);
+            setActiveMenu(false);
         } else {
-        setActiveMenu(true);
+            setActiveMenu(true);
         }
     }, [screenSize]);
 
     return (
-        <div className='nav-container'>
+        <div className='nav-container' >
             <div className="logo-container">
                 <Avatar src={icon} size={64}/>
                 <Typography.Title level={3} className="logo">
                     <Link to='/'>Baniya-Trade</Link>
                 </Typography.Title>
-                <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)} onBlur={() => setActiveMenu(!activeMenu)}> 
+                <Button className="menu-control-container" onClick={() => {setActiveMenu(!activeMenu)}}> 
                     <MenuOutlined />
                 </Button>
             </div>
-            {activeMenu && (
-                <Menu theme='dark' style={{backgroundColor:'black',width: 256}} 
-                    mode="inline">
-                    <Menu.Item key="1" icon={<HomeOutlined/>}>
-                        <Link to='/'>Home</Link>
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<DollarOutlined />}>
-                        <Link to='/Market'>Market</Link>
-                    </Menu.Item>
-                    <Menu.Item key="3" icon={<BulbOutlined/>}>
-                        <Link to='/News'>News</Link>
-                    </Menu.Item>
-                    {(User?.data)?(
-                        <>
-                            <SubMenu key="account" icon={<UserOutlined />} title="My Account">
-                                <Menu.Item key="4" icon={<UserOutlined />}>
-                                    <Link to='/Profile'>Profile</Link>
-                                </Menu.Item>
-                                <Menu.Item key="5" icon={<ProfileOutlined />}>
-                                    <Link to='/Portfolio'>Portfolio</Link>
-                                </Menu.Item>
-                                <Menu.Item key="6" icon={<BankOutlined />}>
-                                    <Link to='/BankOptions'>Payment Options</Link>
-                                </Menu.Item>
-                                <Menu.Item key="7" icon={<ShoppingOutlined />}>
-                                    <Link to='/BuySell'>BuySell</Link>
-                                </Menu.Item>
-                            </SubMenu>
-                            <Menu.Item key="8" icon={<TeamOutlined />}>
-                                <Link to='/AboutUs'> About Us </Link>
-                            </Menu.Item>
+            <div>
+                {(activeMenu) && (
+                    <MenuPanel User={User} setActiveMenu={setActiveMenu}/>
+                )}
+            </div>
 
-                            <Menu.Item key="9" icon={<LogoutOutlined />}>
-                                <Link to='/' onClick={logOut}>LogOut</Link>
-                            </Menu.Item>
-                        </>
-                    ):(
-                        <>
-                            <Menu.Item key="10" icon={<TeamOutlined />}>
-                                <Link to='/AboutUs'> About Us </Link>
-                            </Menu.Item>
-                            <Menu.Item key="11" icon={<UserAddOutlined />}>
-                                <Link to='/Signup'>Sign-Up</Link>
-                            </Menu.Item>
-                            <Menu.Item key="12" icon={<LoginOutlined />}>
-                                <Link to='/Login' on>Login</Link>
-                            </Menu.Item>
-                        </>
-                    )}                
-                </Menu>
-            )}
         </div>
     )
 }
